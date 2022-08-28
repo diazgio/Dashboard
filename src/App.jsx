4db8c34b9806ce0c64c342b390f849a4f28c1fc5
Giordano Díaz <diazgiordano@gmail.com>
2022-08-28 11:38:24 -0500

    Add toggle display to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Pyramid, Line, Editor } from './pages';
+import { useStateContext } from './context/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
+  
   return (
     <div>
       <Router>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,9 +4,10 @@ import { SiShopware } from 'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { links } from '../data/dummy';
+import { useStateContext } from '../context/ContextProvider';
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white txt-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
@@ -14,14 +15,14 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link rel="stylesheet" to="/" onClick={() => {}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+          <Link rel="stylesheet" to="/" onClick={() => setActiveMenu(false)} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
             <SiShopware /> <span>Shoppy</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
             <button
               type="button"
-              onClick={() => {}}
-              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block"
             >
               <MdOutlineCancel />
             </button>
