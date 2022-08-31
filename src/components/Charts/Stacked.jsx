63ee83d79f23194e63cfb10000095ef4066fdbda
Giordano Díaz <diazgiordano@gmail.com>
2022-08-31 15:44:23 -0500

    Finish linea page

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
-
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
 import { useStateContext } from '../../context/ContextProvider';
 
