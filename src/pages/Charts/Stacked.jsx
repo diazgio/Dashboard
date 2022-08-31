fbf8d6637371337ddd33d6bcb3605567ad9dbf07
Giordano Díaz <diazgiordano@gmail.com>
2022-08-31 16:09:28 -0500

    Finish stacked page

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Header, Stacked as StackedChart } from '../../components';
 
 const Stacked = () => {
   return (
-    <div>Stacked</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Stacked" title="Revenue Breakdown" />
+      <div className="w-full">
+        <StackedChart />
+      </div>
+  </div>
   )
 }
 
