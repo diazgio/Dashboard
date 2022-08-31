6933e122e490fbf16d99af234a35e851e3b352da
Giordano Díaz <diazgiordano@gmail.com>
2022-08-31 16:00:14 -0500

    Finish pie chart

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
 import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
-import { ChartsHeader } from '../../components';
+import { Header } from '../../components';
 import { useStateContext } from '../../context/ContextProvider';
 
 const Bar = () => {
@@ -9,7 +9,7 @@ const Bar = () => {
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO" />
+      <Header category="Bar" title="Olympic Medal Counts - RIO" />
       <div className=" w-full">
         <ChartComponent
           id="charts"
